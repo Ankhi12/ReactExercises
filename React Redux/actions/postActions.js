13b22133d5cac8ab9f87f224143ac2dd8c1d3da8
Ankhi12 <124775002+Ankhi12@users.noreply.github.com>
2023-05-06 20:20:37 +0530

    Create postActions.js

---
+++ React Redux/actions/postActions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { POSTSDATA,ERRORDATA } from '../types'
+
+export const getPosts = ()=>async dispatch=>{
+    try{
+        const posts = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        dispatch( {
+            type: POSTSDATA,
+            payload: posts.data
+        })
+    }
+    catch(e){
+        dispatch({
+            type: ERRORDATA,
+            payload: console.log(e)
+        })
+    }
+}
+export const setPosts=(posts)=>{
+    return{
+        type: 'POSTSDATA',
+        payload: posts
+    }
+}
