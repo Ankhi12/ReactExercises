d9f8898234e869b321ad2a974645918f94bdb851
Ankhi12 <124775002+Ankhi12@users.noreply.github.com>
2023-05-10 11:03:02 +0530

    Create App.js

---
+++ order management/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import QuoteFormComp from './component/QuoteFormComp';
+function App() {
+  return (
+    <div className="App">
+      <QuoteFormComp/>
+    </div>
+  );
+}
+export default App;
